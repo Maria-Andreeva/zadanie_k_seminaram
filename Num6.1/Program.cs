2c33bf7dbb32a9e231eba97e3deaafa445b7cb6e
Maria.Andreeva <maria.andreeva.marketing@gmail.com>
2023-11-24 21:57:36 +0100

    added the program. the sum of the first and third digits of a three-digit integer

---
+++ Num6.1/Program.cs
@@ -0,0 +1,23 @@
+﻿//Напишите программу, которая на вход принимает трехзначное целое число, 
+//и на выходе показывает сумму первой и последней цифры этого числа.
+// 456 -> 10
+// 782 -> 9
+// 918 -> 17
+
+Console.WriteLine("Введите трехзначное целое число");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+int temp = num % 10;
+int tem = num / 100;
+int result = temp + tem;
+
+if(num >= 100 && num <= 999)
+{
+    Console.WriteLine(result);
+}
+
+else
+{
+    Console.WriteLine("Неверно указано число");
+}
