93ce621fb374f27c9a2b0502470595202ce5646c
Maria.Andreeva <maria.andreeva.marketing@gmail.com>
2023-11-25 22:30:32 +0100

    table of squares of numbers from 1 to N

---
+++ Num6.3/Program.cs
@@ -0,0 +1,24 @@
+﻿//Напишите программу, которая принимает на вход число (N) и 
+//выдаёт таблицу квадратов чисел от 1 до N.
+//5 -> 1, 4, 9, 16, 25.
+//2 -> 1, 4
+
+
+Console.WriteLine("Введите число");
+
+int N = Convert.ToInt32(Console.ReadLine());
+int temp = 1;
+int i;
+
+for (i=1; i<=N; i++)
+{
+    temp = i*i;
+    Console.Write(temp);
+
+    if(i<N)
+    {
+        Console.Write(", ");
+    }
+}
+
+
