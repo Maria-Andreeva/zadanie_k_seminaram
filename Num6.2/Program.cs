f076a1afce37f8a574505baf12831b56bede308d
Maria.Andreeva <maria.andreeva.marketing@gmail.com>
2023-11-25 22:02:38 +0100

    product from 1 to N

---
+++ Num6.2/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая принимает на вход число N и 
+//выдаёт произведение чисел от 1 до N.
+//4 -> 24 
+//5 -> 120
+
+Console.WriteLine("Введите число");
+
+int N = Convert.ToInt32(Console.ReadLine());
+int temp = 1;
+int i;
+
+for (i=1; i<=N; i++)
+{
+    temp = temp * i;
+}
+
+Console.WriteLine(temp);
