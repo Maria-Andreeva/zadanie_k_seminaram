77a6f0a1a42ca10d8351f1990235548a5f1715d5
Maria Andreeva <maria.andreeva.marketing@gmail.com>
2023-12-01 00:24:45 +0100

    Added Homework number 1

---
+++ N1/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 1: Напишите программу, которая бесконечно запрашивает целые числа с консоли. Программа завершается при вводе символа ‘q’ или при вводе числа, сумма цифр которого чётная.
+//5 12 16 q [STOP]
+//3 45 342 15 [STOP]
+
+while (true)
+{
+    Console.Write("Enter array whole number: ");
+
+    string? temp = Console.ReadLine();
+
+    if (temp != null)
+    {
+        Console.WriteLine(temp);
+
+        if(temp=="q")
+        {
+            break;
+        }
+    
+        int sum = 0;
+
+        for(int i = 0; i < temp.Length; i++)
+        {
+            sum += Convert.ToInt32(temp[i]);
+        }        
+        
+
+        // for(int i = temp.Length -1; i>0; i--)
+        // {
+        //     sum += Convert.ToInt32(temp[i-1]);
+        // }
+
+        if(sum%2==0)
+        {
+            break;
+        }
+    }
+
+}
+
+
