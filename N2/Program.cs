874297117c66a7f8e89755ea1a0c92597f259dba
Maria.Andreeva <maria.andreeva.marketing@gmail.com>
2023-12-03 22:13:16 +0100

    Added DZ number 2

---
+++ N2/Program.cs
@@ -0,0 +1,74 @@
+﻿//Задача 2: Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива.
+
+Console.Clear();
+
+int [,] Create2dArray(int row, int col, int min, int max)
+{
+    int [,] array = new int [row,col];
+    for (int i = 0; i<row; i++)
+    {
+        for(int j = 0; j<col;j++)
+        {
+            array[i,j] = new Random().Next(min, max+1);
+        }
+    }
+    return array;
+}
+
+void Show2dArray(int [,] array)
+{
+    for(int i = 0; i<array.GetLength(0); i++)
+    {
+        for(int j = 0; j<array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j]+" ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int [,] MyArray(int[,] array)
+{
+    int row = array.GetLength(0);
+    int col = array.GetLength(1);
+    int [,] temp = new int[row,col];
+    
+    for(int i = 0; i<array.GetLength(0); i++)
+    {
+        for(int j = 0; j<array.GetLength(1); j++)
+        {
+           if (i==0)
+           {
+            temp [array.GetLength(0)-1,j] = array [i,j];
+           }
+           else if (i==array.GetLength(0)-1)
+           {
+            temp [0,j] = array [i,j];
+           }
+           else 
+           {
+            temp [i,j] = array [i,j];
+           }
+
+        }
+    }
+
+    return temp;
+
+}
+
+Console.WriteLine("Enter row numbers: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter col numbers: ");
+int col = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter min value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter max value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int [,] created2dArray = Create2dArray(row,col,min,max);
+Console.WriteLine();
+Show2dArray(created2dArray);
+Console.WriteLine();
+int [,] changedArr = MyArray(created2dArray);
+Show2dArray(changedArr);
